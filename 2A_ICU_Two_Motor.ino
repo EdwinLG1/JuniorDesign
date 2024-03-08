a6a9ae851be4f10bcea05e60209411ec3306e28a
vicysmalls <120691212+vicysmalls@users.noreply.github.com>
2024-03-08 15:22:19 -0500

    Update 2A_ICU_Two_Motor.ino

--- 2A_ICU_Two_Motor.ino
+++ 2A_ICU_Two_Motor.ino
@@ -1,12 +1,12 @@
 // m in this case means motor 
 // motor 1 is left, motor 2 is right when looking at bot from the back
-int m1forward_pin = A0;
-int m1backward_pin = A1;
-int m2forward_pin = A2;
-int m2backward_pin = A3;
-int mstate_pin = 8;
-int m1control_pin = 7;
-int m2control_pin = 6;
+int state_user_pin = 8; // Input Digital Pin
+int m1_user_pin = 13; // Input Digital Pin
+int m2_user_pin = 12; // Input Digital Pin
+int m1direction_pin = 2; // Output Digital Pin
+int m2direction_pin = 4; // Output Digital Pin
+int m1PWM_pin = 5; // Output Analog Pin
+int m2PWM_pin = 10; // Output Analog Pin
 int max_power = 255;
 int off = 0;
 volatile bool motor_off = true;
@@ -15,8 +15,14 @@ volatile bool m2forward = false;
 volatile bool m1backward = false;
 volatile bool m2backward = false;
 
-void toggle_motor_on(){
-  motor_off = !motor_off;
+void motorON_ISR(){
+  Serial.println("motor on");
+  motor_off = false;
+}
+
+void motorOFF_ISR(){
+  Serial.println("motor off");
+  motor_off = true;
 }
 
 void m1forwardISR(){
@@ -42,46 +48,54 @@ void m2backwardISR(){
 void setup() {
   // This automatically goes into the ON state
   Serial.begin(9600);
-  pinMode(mstate_pin, INPUT_PULLUP);
-  pinMode(m1control_pin, INPUT);
-  pinMode(m2control_pin, INPUT);
-  pinMode(m1forward_pin, OUTPUT);
-  pinMode(m1backward_pin, OUTPUT);
-  pinMode(m2forward_pin, OUTPUT);
-  pinMode(m2backward_pin, OUTPUT);
-  attachInterrupt(mstate_pin, toggle_motor_on, CHANGE);
-  attachInterrupt(m1control_pin, m1forwardISR, HIGH);
-  attachInterrupt(m1control_pin, m1backwardISR, LOW);
-  attachInterrupt(m2control_pin, m2forwardISR, HIGH);
-  attachInterrupt(m2control_pin, m2backwardISR, LOW);
+  pinMode(state_user_pin, INPUT_PULLUP);
+  pinMode(m1_user_pin, INPUT_PULLUP);
+  pinMode(m2_user_pin, INPUT_PULLUP);
+  pinMode(m1direction_pin, OUTPUT);
+  pinMode(m2direction_pin, OUTPUT);
+  pinMode(m1PWM_pin, OUTPUT);
+  pinMode(m2PWM_pin, OUTPUT);
+  attachInterrupt(state_user_pin, motorON_ISR, HIGH);
+  attachInterrupt(state_user_pin, motorOFF_ISR, LOW);
+  attachInterrupt(m1_user_pin, m1forwardISR, HIGH);
+  attachInterrupt(m1_user_pin, m1backwardISR, LOW);
+  attachInterrupt(m2_user_pin, m2forwardISR, HIGH);
+  attachInterrupt(m2_user_pin, m2backwardISR, LOW);
 }
 
 void loop() {
   // MAIN LOOP
   if (motor_off){
-    //nothing
+    off_state();
   }else{
     m1_state_logic();
     m2_state_logic();
   }
 }
 
+void off_state(){
+  // Turn off motors
+  analogWrite(m1PWM_pin, off);
+  analogWrite(m2PWM_pin, off);
+
+}
+
 void m1_state_logic(){
-  if (m1forward){
-    analogWrite(m1backward_pin, off);
-    analogWrite(m1forward_pin, max_power);
-  }else if (m1backward){
-    analogWrite(m1forward_pin, off);
-    analogWrite(m1backward_pin, max_power);
+  if (m1forward){ // Makes motor 1 go to the right
+    digitalWrite(m1direction_pin, HIGH);
+    analogWrite(m1PWM_pin, max_power);
+  }else if (m1backward){ // Makes motor 1 go to the left
+    digitalWrite(m1direction_pin, LOW);
+    analogWrite(m1PWM_pin, max_power);
   }
 }
 
 void m2_state_logic(){
-  if (m2forward){
-    analogWrite(m2backward_pin, off);
-    analogWrite(m2forward_pin, max_power);
-  }else if (m2backward){
-    analogWrite(m2forward_pin, off);
-    analogWrite(m2backward_pin, max_power);
+  if (m2forward){ // Makes motor 2 go to the right
+    digitalWrite(m2direction_pin, HIGH);
+    analogWrite(m2PWM_pin, max_power);
+  }else if (m2backward){ // Makes motor 2 go to the left
+    digitalWrite(m2direction_pin, LOW);
+    analogWrite(m2PWM_pin, max_power);
   }
 }
