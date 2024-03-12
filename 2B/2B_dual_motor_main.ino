b4c9c1a9bc0c3cf7d758a152ad7f60a9e6c5f79e
vicysmalls <120691212+vicysmalls@users.noreply.github.com>
2024-03-11 23:34:50 -0400

    Update 2B_dual_motor_main.ino

--- 2B/2B_dual_motor_main.ino
+++ 2B/2B_dual_motor_main.ino
@@ -1,12 +1,14 @@
 // m in this case means motor 
 // motor 1 is left, motor 2 is right when looking at bot from the back
+#include <MotorClass.h>
+
+// Analog Ouput pins for forward and backward movement
+MotorClass Motor1(2,4);
+MotorClass Motor2(5,10);
+
 int state_user_pin = 8; // Input Digital Pin
 int m1_user_pin = 13; // Input Digital Pin
 int m2_user_pin = 12; // Input Digital Pin
-int m1direction_pin = 2; // Output Digital Pin
-int m2direction_pin = 4; // Output Digital Pin
-int m1PWM_pin = 5; // Output Analog Pin
-int m2PWM_pin = 10; // Output Analog Pin
 int max_power = 255;
 int off = 0;
 volatile bool motor_off = true;
@@ -39,10 +41,6 @@ void setup() {
   pinMode(state_user_pin, INPUT_PULLUP);
   pinMode(m1_user_pin, INPUT_PULLUP);
   pinMode(m2_user_pin, INPUT_PULLUP);
-  pinMode(m1direction_pin, OUTPUT);
-  pinMode(m2direction_pin, OUTPUT);
-  pinMode(m1PWM_pin, OUTPUT);
-  pinMode(m2PWM_pin, OUTPUT);
   attachInterrupt(state_user_pin, motorTOGGLE_ISR, CHANGE);
   attachInterrupt(m1_user_pin, m1directionISR, CHANGE);
   attachInterrupt(m2_user_pin, m2directionISR, CHANGE);
@@ -53,35 +51,26 @@ void loop() {
   digitalWrite(m1direction_pin, HIGH);
   analogWrite(m1PWM_pin, max_power);
   if (motor_off){
-    //off_state();
+    Motor1.stop();
+    Motor2.stop();
   }else{
     m1_state_logic();
     m2_state_logic();
   }
 }
 
-void off_state(){
-  // Turn off motors
-  analogWrite(m1PWM_pin, off);
-  analogWrite(m2PWM_pin, off);
-}
-
 void m1_state_logic(){
   if (m1forward){ // Makes motor 1 go to the right
-    digitalWrite(m1direction_pin, HIGH);
-    analogWrite(m1PWM_pin, max_power);
+    Motor1.goForward(max_power);
   }else if (m1backward){ // Makes motor 1 go to the left
-    digitalWrite(m1direction_pin, LOW);
-    analogWrite(m1PWM_pin, max_power);
+    Motor1.goBackward(max_power);
   }
 }
 
 void m2_state_logic(){
   if (m2forward){ // Makes motor 2 go to the right
-    digitalWrite(m2direction_pin, HIGH);
-    analogWrite(m2PWM_pin, max_power);
+    Motor2.goForward(max_power);
   }else if (m2backward){ // Makes motor 2 go to the left
-    digitalWrite(m2direction_pin, LOW);
-    analogWrite(m2PWM_pin, max_power);
+    Motor2.goBackward(max_power);
   }
 }
