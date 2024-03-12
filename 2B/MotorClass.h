a970bee8523a4a13f6a6c0b15bfda451bf651e21
vicysmalls <120691212+vicysmalls@users.noreply.github.com>
2024-03-11 21:02:42 -0400

    Update MotorClass.h

--- 2B/MotorClass.h
+++ 2B/MotorClass.h
@@ -8,7 +8,7 @@ class MotorClass {
     MotorClass(int forward_pin, int backward_pin);
     void MotorClass::goForward(int speed);
     void MotorClass::goBackward(int speed);
-    void MotorClass::Stop(int speed);
+    void MotorClass::Stop();
   private:
     int _f_pin;
     int _b_pin;
