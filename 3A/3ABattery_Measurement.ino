646233e55b1f16fbbd62d04ed439b5ea839a1efc
Aybas <95939945+Aybas@users.noreply.github.com>
2024-04-09 13:10:34 -0400

    Update 3ABattery_Measurement.ino

--- 3A/3ABattery_Measurement.ino
+++ 3A/3ABattery_Measurement.ino
@@ -1,1 +1,13 @@
 // Battery Power Management Measuring System
+Void setup( ) {
+	pinMode(2, OUTPUT)
+	pinMode(3, OUTPUT)
+	pinMode(4, OUTPUT)
+	digitalWrite(2, LOW)
+	digitalWrite(3, LOW)
+	digitalWrite(4, LOW)
+}
+
+Void batteryVoltage(){
+	int batterySensor = analogRead
+}
