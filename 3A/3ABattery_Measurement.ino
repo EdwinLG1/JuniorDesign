c3dcc07d9c01ee6b42c9ab6b2c4e8dda239b9ff1
Aybas <95939945+Aybas@users.noreply.github.com>
2024-04-09 13:40:02 -0400

    Update 3ABattery_Measurement.ino

--- 3A/3ABattery_Measurement.ino
+++ 3A/3ABattery_Measurement.ino
@@ -1,13 +1,22 @@
 // Battery Power Management Measuring System
-void setup( ) {
-	pinMode(2, OUTPUT)
-	pinMode(3, OUTPUT)
-	pinMode(4, OUTPUT)
-	digitalWrite(2, LOW)
-	digitalWrite(3, LOW)
-	digitalWrite(4, LOW)
+void setup() {
+	pinMode(2, OUTPUT); // Red LED
+	pinMode(3, OUTPUT); // Yellow LED
+	pinMode(4, OUTPUT); // Green LED
+	digitalWrite(2, LOW);
+	digitalWrite(3, LOW);
+	digitalWrite(4, LOW);
+  Serial.begin(9600);
+}
+
+void loop(){
+  batteryVoltage();
 }
 
 void batteryVoltage(){
-	int batterySensor = analogRead(A3)
+	int batterySensor = analogRead(A3);
+  Serial.println(batterySensor);
+  if((batterySensor >= 900) && (batterySensor <= 1023)){
+    digitalWrite(4, HIGH);
+  }
 }
