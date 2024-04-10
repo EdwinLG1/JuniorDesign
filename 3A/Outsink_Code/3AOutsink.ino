a6746f8304687320d2b7ea16687cb557fac54354
vicysmalls <120691212+vicysmalls@users.noreply.github.com>
2024-04-10 19:04:44 -0400

    Update 3AOutsink.ino

--- 3A/Outsink_Code/3AOutsink.ino
+++ 3A/Outsink_Code/3AOutsink.ino
@@ -2,6 +2,14 @@
 // motor 1 is left, motor 2 is right when looking at bot from the back
 
 int ir_transistor = A0; // Analog Input Pin
+int collision_pin = 5; // Digital Input Pin
+int collision_initiate_pin = 6; // Dig In Pin || to trigger interrupt
+int motorAPin1 = 7
+int motorAPin2 = 8
+int motorBPin1 = 9
+int motorBPin2 = 10
+int enableAPin1 = 11
+int enableAPin2 = 12
 int max_power = 255;
 int off = 0;
 int collision_range[2] = {0,500}; // range of acceptable voltage to detect collision
@@ -19,7 +27,9 @@ void setup() {
   // This automatically goes into the ON state
   Serial.begin(9600);
   pinMode(ir_transistor, INPUT);
-  attachInterrupt(ir_transistor, collect_ir_data, RISING);
+  pinMode(collision_pin, INPUT);
+  pinMode(collision_initiate_pin, OUTPUT);
+  attachInterrupt(collision_pin, collision, RISING);
 }
 
 
@@ -39,8 +49,10 @@ void collision_logic(){
   ir_value = analogRead(ir_transistor);
   Serial.println(ir_value);
   if (ir_value >= collision_range[0] && ir_value <= collision_range[1]){
+    digitalWrite(collision_initiate_pin, HIGH);
     CURRENT_STATE = STOP_STATE;
   }else{
+    digitalWrite(collision_initiate_pin, LOW);
     CURRENT_STATE = FORWARD_STATE;
 }
 
