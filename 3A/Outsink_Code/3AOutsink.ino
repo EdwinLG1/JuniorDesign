feb29edf35fed5423d5801cd532af6bf54166fd2
vicysmalls <120691212+vicysmalls@users.noreply.github.com>
2024-04-10 15:22:44 -0400

    Update 3AOutsink.ino

--- 3A/Outsink_Code/3AOutsink.ino
+++ 3A/Outsink_Code/3AOutsink.ino
@@ -11,6 +11,10 @@ int FORWARD_STATE = 1
 int STOP_STATE = 2;
 int CURRENT_STATE = STEERING_STATE;
 
+void collision(){
+  stopMotors()
+}
+
 void setup() {
   // This automatically goes into the ON state
   Serial.begin(9600);
@@ -18,33 +22,47 @@ void setup() {
   attachInterrupt(ir_transistor, collect_ir_data, RISING);
 }
 
+
 void loop() {
   // MAIN LOOP
   if (CURRENT_STATE == STOP_STATE){
-    
-    CURRENT_STATE = TURN_STATE;
-  }else if (CURRENT_STATE == STEERING_STATE){
     collision_logic();
-    color_logic();
-    steering_logic();
+  }else if (CURRENT_STATE == FORWARD_STATE){
+    collision_logic();
   }else if (CURRENT_STATE == TURN_STATE){
     turnCounterClockwise(180); // 180 degree turn
     CURRENT_STATE = STEERING_STATE;
   }
 }
 
-void collision(){
+void collision_logic(){
   ir_value = analogRead(ir_transistor);
   Serial.println(ir_value);
-}
-
-void collision_logic(){
   if (ir_value >= collision_range[0] && ir_value <= collision_range[1]){
     CURRENT_STATE = STOP_STATE;
-  }
+  }else{
+    CURRENT_STATE = FORWARD_STATE;
+}
+
+void forwardDirection() {
+  digitalWrite(motorAPin1, HIGH);
+  digitalWrite(motorAPin2, LOW);
+  
+  digitalWrite(motorBPin1, LOW);
+  digitalWrite(motorBPin2, HIGH);
+
+  analogWrite(enableAPin1, 150); // Set speed (0 to 255)
+  analogWrite(enableBPin1, 150);
 }
 
 void stopMotors(){
+  digitalWrite(motorAPin1, LOW);
+  digitalWrite(motorAPin2, LOW);
+  
+  digitalWrite(motorBPin1, LOW);
+  digitalWrite(motorBPin2, LOW);
 
+  analogWrite(enableAPin1, 0); // Set speed (0 to 255)
+  analogWrite(enableBPin1, 0);
 }
 
