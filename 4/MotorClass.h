30d8e5755ce49807e598f9404dd32881de7543b5
vicysmalls <120691212+vicysmalls@users.noreply.github.com>
2024-05-06 20:10:35 -0400

    Add files via upload

---
+++ 4/MotorClass.h
@@ -0,0 +1,18 @@
+#ifndef MotorClass_h
+#define MotorClass_h
+
+#include "Arduino.h"
+
+class MotorClass {
+  public:
+    MotorClass(int forward_pin, int backward_pin);
+    void MotorClass::goForward(int speed);
+    void MotorClass::goBackward(int speed);
+    void MotorClass::stop();
+  private:
+    int _forward_pin;
+    int _backward_pin;
+    int _OFF ;
+};
+
+#endif
